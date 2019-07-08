f75f639133b6d1c6e71224f2c05ef27dfd470a7b
John Ragbir <johnragbir@gmail.com>
2019-07-08 10:47:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ function sayHiToGrandma(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase());
-  return ("I can't hear you!")
+  if ("I can't hear you!");
+  return ("YES INDEED!")
 }
 
 
